f347161288ecdeb9c4948371dfbc956d33508d39
NANDHAKUMARC2002 <111556821+NANDHAKUMARC2002@users.noreply.github.com>
2022-08-22 15:51:53 +0530

    Update pg1.c

--- pg1.c
+++ pg1.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int main(){
+    int i,j,n=5;
+    for(i=1;i<=n;i++){
+        for(j=5;j>=n-i+j;j--)
+        {
+            if((i==j)||(n==i+j-1))
+            {
+                printf("%d",j);
+            }
+            else
+            {
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+}
